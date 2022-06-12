827d89a0203f5b78ea24954ff90205c57898eab8
Thiago Brumer <thiagobrumer@gmail.com>
2022-06-12 12:52:02 -0300

    init

---
+++ src/test/java/br/pucminas/aws/AwsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.pucminas.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
